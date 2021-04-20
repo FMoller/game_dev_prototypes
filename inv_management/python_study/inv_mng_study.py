46a7d4f97b6370f26cd9c6b6230ece29f2b36c7a
FMoller <fredericomollerper@gmail.com>
2021-04-20 17:22:27 -0300

    Create inv_mng_study.py

---
+++ inv_management/python_study/inv_mng_study.py
@@ -0,0 +1,25 @@
+"""
+Simple study about a inventory management system
+"""
+
+__version__ = "0.1"
+__author__ = "Frederico Moeller"
+__copyright__ = "Copyright 2021, Frederico Moeller"
+__license__ = "MIT"
+
+
+import numpy as np
+
+
+class Inventory():
+    """ Class that defines player's inventory """
+
+    def __init__(self, rows, cols):
+
+        self.lines = lines
+        self.cols = cols
+        self.area = [[None for j in cols] for i in rows]
+    
+
+
+
