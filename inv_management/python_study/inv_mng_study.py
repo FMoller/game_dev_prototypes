9e87500d622e192b3a99007c90a5a15cc03a027e
FMoller <fredericomollerper@gmail.com>
2021-04-22 00:12:26 -0300

    Update inv_mng_study.py

--- inv_management/python_study/inv_mng_study.py
+++ inv_management/python_study/inv_mng_study.py
@@ -23,22 +23,42 @@ class Inventory():
         self.area = np.zeros((rows,cols))
         self.key_i = 1
         self.key_s = rows*cols+1
+        self.on_tranf = None
 
-    def find_space(self,space):
+    def find_space_old(self, space): #O(N**2 * M**2)
+        """ Find the location of a free space in the inventory""" 
         comparing = np.zeros(space)
-        for i in range(self.rows-space[0]):
-            for j in range(self.cols-space[1]):
+        for i in range(self.rows-space[0]+1):
+            for j in range(self.cols-space[1]+1):
                 if False not in (self.area[i:i+space[0],j:j+space[1]] == comparing):
                     return [i,j]
         return [-1,-1]
 
-    def add_object(self, obj):
+    def find_space(self, space): #O(N**2 * M)
+        """ Find the location of a free space in the inventory"""
+        comparing = np.zeros(space)
+        for i in range(self.rows-space[0]+1):
+            for j in range(self.cols-space[1]+1):
+                if False not in (self.area[i,j:j+space[1]] == comparing[0,:]):
+                    if False not in (self.area[i:i+space[0],j:j+space[1]] == comparing):
+                        return [i,j]
+        return [-1,-1]
+
+    def add_object(self, objc):
+        """ Add a new object in the inventory"""
+        obj = objc.copy()
         if obj.stackable:
             for i in self.stk.values():
                 if i.base_id == obj.base_id:
                     if i.stack < i.max_stack:
-                        i.stack += 1
-                        break
+                        if obj.stack + i.stack <i.max_stack:
+                            print(obj.stack + i.stack)
+                            i.stack += obj.stack
+                            break
+                        else:
+                            obj.stack -= (i.max_stack - i.stack)
+                            i.stack = i.max_stack
+                            
             else:
                 place = self.find_space(obj.space)
                 if -1 in place:
@@ -46,7 +66,7 @@ class Inventory():
                     return False
                 else:
                     obj.pos_x = place[0]
-                    obj.pos_x = place[1]
+                    obj.pos_y = place[1]
                     self.stk[self.key_s]=obj
                     self.area[place[0]:place[0]+obj.space[0],
                               place[1]:place[1]+obj.space[1]] = self.key_s
@@ -56,10 +76,11 @@ class Inventory():
             place = self.find_space(obj.space)
             if -1 in place:
                 print('No space in inventory')
-                return None
+                return False
             else:
+                obj = obj.copy()
                 obj.pos_x = place[0]
-                obj.pos_x = place[1]
+                obj.pos_y = place[1]
                 self.inv[self.key_i]=obj
                 self.area[place[0]:place[0]+obj.space[0],
                           place[1]:place[1]+obj.space[1]] = self.key_i
@@ -69,9 +90,70 @@ class Inventory():
 
                     
     def find_newks(self):
-        pass
+        """ Find new key to stackable itens"""
+        for i in range(self.key_s+1,2*self.rows*self.cols+1):
+            if i not in self.stk.keys():
+                self.key_s = i
+                break
+        else:
+            for i in range(self.rows*self.cols+1,self.key_s):
+                self.key_s = i
+                break
+            else:
+                print("Something wrong is not right in da key_s allocation")
+                
     def find_newki(self):
-        pass
+        """ Find new key to inventory itens"""
+        for i in range(self.key_i+1,self.rows*self.cols+1):
+            if i not in self.inv.keys():
+                self.key_i = i
+                break
+        else:
+            for i in range(1,self.key_i):
+                if i not in self.inv.keys():
+                    self.key_i = i
+                    break
+            else:
+                print("Something wrong is not right in da key_s allocation")
+                
+    def remove_item(self, place, qtt=1):
+        """ Remove a iten from the inventory"""
+        key = self.area[place[0], place[1]]
+        print(key)
+        if key == 0:
+            return False
+        elif key < self.rows*self.cols+1:
+            self.relase_space(self.inv[key])
+            pivot = self.inv[key].copy()
+            del(self.inv[key])
+            return pivot
+        else:
+            print(self.stk[key].stack)
+            if self.stk[key].stack-qtt <= 0:
+                self.relase_space(self.stk[key])
+                pivot = self.stk[key].copy()
+                del(self.stk[key])
+                return pivot
+            else:
+                print('foi')
+                self.stk[key].stack -= qtt
+                pivot = self.stk[key].copy()
+                pivot.stack = qtt
+                return pivot
+
+    def relase_space(self, obj):
+        """ Clear inv space"""
+        print(obj.pos_x)
+        self.area[obj.pos_x:obj.pos_x+obj.space[0],
+                  obj.pos_y:obj.pos_y+obj.space[1]] = 0
+
+##    def rc_mouse(self,place):
+##        
+##        if self.on_tranf == None:
+##            self.on_tranf = self.remove_item(place)
+##        else:
+##            if self.on_tranf.stackable:
+##                if self
                     
                     
                         
@@ -89,7 +171,11 @@ class Object():
         self.max_stack = max_stack
         self.base_id = base_id
         self.pos_x = 0
-        self_pos_y = 0
+        self.pos_y = 0
+
+    def copy(self):
+        return Object(self.name, (self.space[0],self.space[1]), self.stackable, self.weight,
+                      self.base_id, self.max_stack, self.stack)
             
 
 
